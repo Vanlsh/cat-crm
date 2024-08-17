0a7a1fac1c0da3aacdc70e988c0f24d24a55be98
Vanlsh <92462786+Vanlsh@users.noreply.github.com>
2024-08-17 23:20:55 +0300

    first commit

---
+++ dist/assets/index-d2e92084.js
@@ -0,0 +1,51 @@
+(function polyfill() {
+  const relList = document.createElement("link").relList;
+  if (relList && relList.supports && relList.supports("modulepreload")) {
+    return;
+  }
+  for (const link of document.querySelectorAll('link[rel="modulepreload"]')) {
+    processPreload(link);
+  }
+  new MutationObserver((mutations) => {
+    for (const mutation of mutations) {
+      if (mutation.type !== "childList") {
+        continue;
+      }
+      for (const node of mutation.addedNodes) {
+        if (node.tagName === "LINK" && node.rel === "modulepreload")
+          processPreload(node);
+      }
+    }
+  }).observe(document, { childList: true, subtree: true });
+  function getFetchOpts(link) {
+    const fetchOpts = {};
+    if (link.integrity)
+      fetchOpts.integrity = link.integrity;
+    if (link.referrerPolicy)
+      fetchOpts.referrerPolicy = link.referrerPolicy;
+    if (link.crossOrigin === "use-credentials")
+      fetchOpts.credentials = "include";
+    else if (link.crossOrigin === "anonymous")
+      fetchOpts.credentials = "omit";
+    else
+      fetchOpts.credentials = "same-origin";
+    return fetchOpts;
+  }
+  function processPreload(link) {
+    if (link.ep)
+      return;
+    link.ep = true;
+    const fetchOpts = getFetchOpts(link);
+    fetch(link.href, fetchOpts);
+  }
+})();
+function foo(...args) {
+  console.log(this);
+  console.log(args);
+}
+const foo2 = foo.bind({ name: "ivam" }, 3);
+const foo3 = foo2.bind({ name: "ivan" }, 2);
+foo3();
+const obj = /* @__PURE__ */ Object.create(null);
+obj.name = "ivan";
+console.log(obj);
