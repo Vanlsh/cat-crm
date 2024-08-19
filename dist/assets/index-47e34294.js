52b61a120ee6cd61a6c1691ee69fc00784c34da9
Vanlsh <92462786+Vanlsh@users.noreply.github.com>
2024-08-19 22:59:24 +0300

    add fonts && icons

--- dist/assets/index-47e34294.js
+++ dist/assets/index-47e34294.js
@@ -40,3 +40,23 @@
   }
 })();
 const main = "";
+const refsMenu = {
+  openMenuButton: document.querySelector(".js-menu-open"),
+  closeMenuButton: document.querySelector(".js-menu-close"),
+  overlayMenu: document.querySelector(".js-menu")
+};
+const toggleMenu = () => {
+  const isOpenMenu = refsMenu.openMenuButton.getAttribute("aria-expanded") === "true" || false;
+  refsMenu.closeMenuButton.setAttribute("aria-expanded", !isOpenMenu);
+  refsMenu.overlayMenu.classList.toggle("is-open");
+  document.body.style.overflow = isOpenMenu ? "hidden" : "auto";
+};
+toggleMenu.openMenuButton.addEventListener("click", toggleMenu);
+toggleMenu.closeMenuButton.addEventListener("click", toggleMenu);
+window.matchMedia("(min-width: 1200px)").addEventListener("change", (event) => {
+  if (!event.matches)
+    return;
+  refsMenu.closeMenuButton.setAttribute("aria-expanded", false);
+  refsMenu.overlayMenu.classList.remove("is-open");
+  document.body.style.overflow = "auto";
+});
